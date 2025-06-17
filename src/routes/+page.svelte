49b2f4911cc5d13b53b2f3adca40f2b86c3244f6
Vitor Mignoni <vichsort@proton.me>
2025-06-17 11:14:17 -0300

    teste inicial 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import InitialText from '$lib/InitialText.svelte';
+    import '../global.css';
+</script>
+
+<InitialText>Olá mundo!</InitialText>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
