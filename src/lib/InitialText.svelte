c33d0797598b46f5f60321d5c5f38f0d4271abf9
Vitor Mignoni <vichsort@proton.me>
2025-06-17 11:21:54 -0300

    quase la

--- src/lib/InitialText.svelte
+++ src/lib/InitialText.svelte
@@ -1,1 +1,22 @@
 <h1><slot></slot></h1>
+
+<script>
+    let accumulatedScroll = 0;
+    import { onMount } from 'svelte';
+    import { gsap } from 'gsap';
+    let visible = true;
+    let slotElement;
+
+    onMount(() => {
+        window.addEventListener('wheel', (e) => {
+            accumulatedScroll += e.deltaY;
+            console.log(accumulatedScroll)
+
+            if (accumulatedScroll >= 270) {
+                gsap.to(slotElement, { duration: 0.8, opacity: 0, y: 100, ease: "back.in" });
+            } else if (accumulatedScroll <= -90) {
+                gsap.to(slotElement, { duration: 0.8, opacity: 10, y: 0, ease: "back.out" });
+            }
+        });
+    });
+</script>
